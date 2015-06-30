e71350f6490cd44899e895c02b0184a94916b44e
System Administrator <root@kum-rhonea2-6.kumc.edu>
2015-06-30 10:27:35 -0500

    Cleaned up, final version (with debugging output)

--- fitness.php
+++ fitness.php
@@ -58,57 +58,50 @@
     }
 </style>
 
-	<!--[if IE]>
-	<style type="text/css">
-		/* Or here, hopefully */
-	</style>
-	<![endif]-->
-
 </head>
 <body>
 
 <div class="container">
   <div class="bs-callout bs-callout-info" id="callout-alerts-no-default">
     <h4 class="section-header text-center">Fitness Ranking by the KU Alzheimers Disease Center </h4>
-	<br />    
-    <p>The rate of oxygen consumption during intense aerobic exercise - for instance, while using a treadmill at an exhaustive pace - is a vital determinant in a person's maximum endurance capacity for aerobic fitness during prolonged, sub-maximal aerobic exercise. Typically, this measure of aerobic fitness is denoted as one's <strong>VO<sub>2</sub> max</strong> score, which represents the maximum volume (V) of oxygen (O2) that one can use in one minute while breathing air at normal barometric pressure.</p>
-    <p>This is measured in milliters (ml) of oxygen consumed per kilogram (kg) of body weight per minute (min):</p>
+	<br /><br />
+    <p>A person's maximum rate of oxygen consumption during aerobic exercise, their <strong>VO<sub>2</sub> max</strong>, is the gold standard of measurement in aerobic fitness scoring. It is often normalized to a person's body weight to allow for comparison between different people. However, other factors aside from body weight, such as age, can also influence an individual's VO<sub>2</sub> max score. This calculator converts a VO2 max score to a percentile rank, so people of different ages can be compared.</p><br />
+	<p>The form fields below will ask you to enter your age, your VO<sub>2</sub> max score from a recent maximal exercise test, and your sex. From this information, the fitness calculator will generate a percentile rank representing your overall fitness, comparing your VO<sub>2</sub> max to those of others in your peer group.</p><br />
+    <p>Essentially, a VO<sub>2</sub> max score represents the maximum volume (V) of oxygen (O<sub>2</sub>) that a person can consume within one minute while performing intense aerobic exercise and breathing air at normal barometric pressure. This is measured in milliters (ml) of oxygen consumed per kilogram (kg) of body weight per minute (min):</p>
 	<p class="text-center"><strong>ml <i class="fa fa-times"></i> kg<sup>1</sup> <i class="fa fa-times"></i> min<sup>1</sup></strong></p>
-	<p>The form fields below will ask you to enter your age, your VO<sub>2</sub> max score from a recent maximal exercise test, and your sex. From this information, the fitness calculator will generate a percentile rank representing your overall fitness, comparing your VO2max score to those of others in the same age/sex demographics as you.</p>
-    <p>This application was created as an effort of the <a href="http://www.kualzheimers.org/" target="_blank">Alzheimer's Disease Center</a>. The algorithm used for these rankings is based on that published by the <a href="http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4139760/" target="_blank">American College of Sports Medicine Guidelines for Exercise Testing and Prescription, 9th Ed.</a> and is currently being peer reviewed. Maximal exercise testing should be conducted on a treadmill, according to ACSM guidelines and safety recommendations. </p><br />
-    <p></p>
+	<p>Additional information on the metabolic equations used to calculate VO<sub>2</sub> max scores can be found via the <a href="http://certification.acsm.org/metabolic-calcs" target="_blank">ACSM website</a>.</p><br /><br />
+    <p>This application was created as an effort of the <a href="http://www.kualzheimers.org/" target="_blank">Alzheimer's Disease Center</a>. The algorithm used for these rankings is based on that published by the <a href="http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4139760/" target="_blank">American College of Sports Medicine Guidelines for Exercise Testing and Prescription, 9th Ed.</a> and is currently being peer reviewed. Maximal exercise testing should be conducted on a treadmill, according to <a href="http://www.acsm.org/" target="_blank">ACSM</a> guidelines and safety recommendations. </p><br />
     <div class="alert alert-success" role="alert"><p>Please note that these figures are estimates only. Your information will not be stored by this application, as the fitness calculator exists for demonstrative purposes only.</div>
-  </div><br /><br /><br />
+  </div> <!-- End #callout-alerts-no-default .bs-callout .bs-callout-info -->
+  <br /><br /><br />
 
-<div class="row">
-  <div class="col-xs-12 col-sm-6 col-md-8">
-<form action="" id="calcform">
-  <div class="form-group">
-      <label for="ageInput">What is your age? <small>(In years)</small></label>
-    <input type="number" min="0" class="form-control" name="age" id="ageInput" placeholder="For example: 41">
-  </div>
-  <div class="form-group">
-      <label for="vomaxInput">What is your current VO<sub>2</sub> max score? <small>(This number can be entered with or without a decimal value)</small></label>
-    <input type="number" min="0.00" max="120.00" class="form-control" name="vomax" id="vomaxInput" placeholder="For example: 32.9">
-  </div>
-  <div class="select">
-    <label for="sexInput">What is your sex?</label>
-    <select class="form-control" id="sexInput">
-      <option>Select one</option>
-      <option value="m" id="m">Male</option>
-      <option value="f" id="f">Female</option>
-    </select>
-  </div>
-  <br />
-  <button class="btn btn-primary" type="button" id="calc">Calculate</button><br /><br />
-  <div id="fitnessRank"></div>
-  <p id="debugOutput"></p>
-</form>
-	<p id="result"></p>
-  </div> <!-- End .col-xs-12 .col-sm-6 .col-md-8 -->
-</div> <!-- End .row -->
-<br /><br /><br /><br /><br /><br />
-</div> <!-- .container end -->
+  <div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-8">
+	  <form id="calcform">
+  		<div class="form-group">
+      	  <label for="ageInput">What is your age? <small>(In years)</small></label>
+      	  <input type="number" min="0" class="form-control" name="age" id="ageInput" placeholder="For example: 41">
+  		</div> <!-- End .form-group -->
+  		<div class="form-group">
+      	  <label for="vomaxInput">What is your current VO<sub>2</sub> max score? <small>(This number can be entered with or without a decimal value)</small></label>
+      	  <input type="number" min="0.00" max="120.00" class="form-control" name="vomax" id="vomaxInput" placeholder="For example: 32.9">
+  		</div> <!-- End .form-group -->
+  		<div class="select">
+      	  <label for="sexInput">What is your sex?</label>
+      	  <select class="form-control" id="sexInput">
+      	    <option>Select one</option>
+      	    <option value="m" id="m">Male</option>
+      	    <option value="f" id="f">Female</option>
+      	  </select>
+  		</div> <!-- End .select -->
+  		<br />
+  		<button class="btn btn-primary" type="button" id="calc">Calculate</button><br /><br />
+  		<div id="fitnessRank"></div>
+	  </form>
+    </div> <!-- End .col-xs-12 .col-sm-6 .col-md-8 -->
+  </div> <!-- End .row -->
+  <br /><br /><br /><br /><br /><br />
+</div> <!-- End .container -->
 
 
 
@@ -126,6 +119,7 @@ $("#calc").on("click", function(){
             var d = 0.2091;
             var e = 0.001177 * age;
             var f = -0.000006232 * (Math.pow(age, 2));
+        // Need to find a less redundant way to declare all variables from here down (including the HTML insert), since these are universal variables, but can't declare the variable expressions prior to declaring their requisite variables (vars. a-f)
             var p1 = a + b + c;
             var p2 = (d + e + f) * vomax;
             var x = (-1) * (p1 + p2);
@@ -135,9 +129,6 @@ $("#calc").on("click", function(){
             $("#fitnessRank").html(
                 "<div class='panel panel-success'><div class='panel-heading'><h3 class='panel-title'>Your Results:</h3></div><p class='panel-body'><br />The percentile rank of your overall fitness score is <strong>" + rank +"%</strong>, compared to your peers. <br /><br />This means that for every 100 people with whom you share the above attributes, there will be approximately " + parseInt(rank) + " people who will have a fitness score that is the same as yours or lower.<br /><br /></p></div>"
                 // Alternatively, we could use "Math.round(rank)" instead of "parseInt(rank)", depending on whether it would be more appropriate to round a percentile up or down...
-        );
-            $("#debugOutput").html(
-                "<br /><br /><br /><br /><div class='panel panel-default'><div class='panel-heading'><h3 class='panel-title'>Debugging output:</h3></div><p class='panel-body'><br />a = " + a + " <br />b = " + b + "<br />c = " + c + "<br />d = " + d + "<br />e = " + e + "<br />f = " + f + "<br />p1 = " + p1 + " <br /> p2 = " + p2 + "<br /> x = "+ x +"<br /> logout = "+logout+"<br /> ranklong = "+ranklong+"<br /><br /></p></div>"
         );};
         break;
            
@@ -148,6 +139,7 @@ $("#calc").on("click", function(){
             var d = 0.2502;
             var e = -1 * (0.001242 * age);
             var f = 0.00004126 * (Math.pow(age, 2));
+        // Need to find a less redundant way to declare all variables from here down (including the HTML insert), since these are universal variables, but can't declare the variable expressions prior to declaring their requisite variables (vars. a-f)
             var p1 = a + b + c;
             var p2 = (d + e + f) * vomax;
             var x = (-1) * (p1 + p2);
@@ -157,9 +149,6 @@ $("#calc").on("click", function(){
             $("#fitnessRank").html(
                 "<div class='panel panel-success'><div class='panel-heading'><h3 class='panel-title'>Your Results:</h3></div><p class='panel-body'><br />The percentile rank of your overall fitness score is <strong>" + rank +"%</strong>, compared to your peers. <br /><br />This means that for every 100 people with whom you share the above attributes, there will be approximately " + parseInt(rank) + " people who will have a fitness score that is the same as yours or lower.<br /><br /></p></div>"
                 // Alternatively, we could use "Math.round(rank)" instead of "parseInt(rank)", depending on whether it would be more appropriate to round a percentile up or down...
-        );
-            $("#debugOutput").html(
-                "<br /><br /><br /><br /><div class='panel panel-default'><div class='panel-heading'><h3 class='panel-title'>Debugging output:</h3></div><p class='panel-body'><br />a = " + a + " <br />b = " + b + "<br />c = " + c + "<br />d = " + d + "<br />e = " + e + "<br />f = " + f + "<br />p1 = " + p1 + " <br /> p2 = " + p2 + "<br /> x = "+ x +"<br /> logout = "+logout+"<br /> ranklong = "+ranklong+"<br /><br /></p></div>"
         );};
         break;
     }
